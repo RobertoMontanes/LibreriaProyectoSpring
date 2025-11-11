28212e3f9676d87b256a0e11560e165b5fdce964
Roberto <rmr15092004@gmail.com>
2025-11-11 12:25:05 +0100

    Proyecto Spring 11/11/2025

---
+++ Proyecto_examen_final/src/main/java/com/salesianostriana/dam/libreria/controller/AutorController.java
@@ -0,0 +1,10 @@
+package com.salesianostriana.dam.libreria.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/autores")
+public class AutorController {
+ 
+}
